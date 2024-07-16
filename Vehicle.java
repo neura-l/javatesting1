c3831b1e0b86873d49f790921ad0635312fdb367
ilham <ilham@debian.debian>
2024-07-17 01:07:05 +0700

    my commit

---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+public class Vehicle {
+    private String name;
+    private int cc;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
